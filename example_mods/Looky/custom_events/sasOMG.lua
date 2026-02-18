33ccb69b36e2726d1905792a5e8778a5354b0db5
LookyReviewsYouTube <lookylore@gmail.com>
2026-02-18 18:12:48 +0000

    remove useless events

--- example_mods/Looky/custom_events/sasOMG.lua
+++
@@ -1,7 +0,0 @@
-function onEvent(name, value1, value2)
-    if name == "sasOMG" then
-		setProperty('cameraSpeed', getProperty('cameraSpeed') - value2)
-        setProperty('cameraSpeed', getProperty('cameraSpeed') + value1)
-    end
-end
-
